e512c72017284a4762491446fbae8b648dfe6b51
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-02-27 21:43:52 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-"SELECT AVG(gpa) FROM syudenst"
+"SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
-  "SELECT Sum(tardies) From studenst"
+  "SELECT Sum(tardies) From student"
 end
 
 def average_gpa_for_9th_grade
